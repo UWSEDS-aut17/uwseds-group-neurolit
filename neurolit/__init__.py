ca478ddb681e850078b3a38b83af157a970828c3
Sritam Kethireddy <kethirs@uw.edu>
2017-12-04 09:29:40 -0800

    Added a bunch of init files

--- neurolit/__init__.py
+++ neurolit/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ["analysis", "base"]
+__all__ = ['analysis', 'base']
 
 # Easier access to key modules/classes
 from .base.dataset import Dataset
 from .base import dataset
 from .analysis import reduce
+from .analysis import classify
