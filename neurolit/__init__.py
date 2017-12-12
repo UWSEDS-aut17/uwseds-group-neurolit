6aae21aa00cdaa38de0d8a4d909761cb3129d820
Sritam Kethireddy <kethirs@uw.edu>
2017-12-12 09:57:29 -0800

    Added some tests for classifiers and more documentation

--- neurolit/__init__.py
+++ neurolit/__init__.py
@@ -2,6 +2,7 @@ __all__ = ['analysis', 'base']
 
 # Easier access to key modules/classes
 from .base.dataset import Dataset
+from .analysis.classify import Classifier
 from .base import dataset
 from .analysis import reduce
 from .analysis import classify
